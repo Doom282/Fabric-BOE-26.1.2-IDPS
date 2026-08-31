35ac5a3bd130302da7e251e1a79dadaecf754e8c
DoomXD <maciain2009@gmail.com>
2026-08-30 20:34:14 -0400

    FIRST

---
+++ src/main/java/net/doomxd/boe/BeyondOnesEyesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.doomxd.boe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeyondOnesEyesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
