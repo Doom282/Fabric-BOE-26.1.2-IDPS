a92847549232228c9c73d3fe24bc903bc0ee5574
DoomXD <maciain2009@gmail.com>
2026-09-16 13:49:54 -0400

    proved ber (block entity renderer) works, via writing "boop" also learned that matrices.pushPose() crashes the client

--- src/main/java/net/doomxd/boe/block/entity/renderer/ElsewhereBlockEntityRenderer.java
+++ src/main/java/net/doomxd/boe/block/entity/renderer/ElsewhereBlockEntityRenderer.java
@@ -1,7 +1,11 @@
 package net.doomxd.boe.block.entity.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.doomxd.boe.block.ModBlocks;
+import net.doomxd.boe.block.custom.ElsewhereBlock;
 import net.doomxd.boe.block.entity.custom.ElsewhereBlockEntity;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
@@ -10,8 +14,11 @@ import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.Nullable;
+
+import static java.awt.SystemColor.text;
 
 public class ElsewhereBlockEntityRenderer implements BlockEntityRenderer<ElsewhereBlockEntity, ElseworldBlockEntityRenderState> {
 
@@ -29,12 +36,35 @@ public class ElsewhereBlockEntityRenderer implements BlockEntityRenderer<Elsewhe
                                    ElseworldBlockEntityRenderState state, float partialTicks, Vec3 cameraPosition,
                                    ModelFeatureRenderer.@Nullable CrumblingOverlay breakProgress) {
         BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTicks, cameraPosition, breakProgress);
-
         state.level = blockEntity.getLevel();
+
+
     }
 
     @Override
-    public void submit(ElseworldBlockEntityRenderState state, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState camera) {
-            
+    public void submit(ElseworldBlockEntityRenderState state, PoseStack matrices, SubmitNodeCollector queue, CameraRenderState cameraState) {
+        //queue.submitBlockModel(matrices, ModBlocks.ELSEWHERE_BLOCK., 1, 1, 1, 1, 1);
+
+        String text = "boop";
+        float width = 0.01f;
+        //matrices.pushPose(); CRASHES THE CLIENT, DONT KNOW WHY
+        matrices.translate(0.5, 1, 0.5);
+        matrices.mulPose(Axis.XP.rotationDegrees(90));
+        matrices.scale(1/18f, 1/18f, 1/18f);
+
+// draw the text. params:
+// text, x, y, color, ordered text, shadow, text layer type, light, color, background color, outline color
+        queue.submitText(
+                matrices,
+                -width / 2, -1f,
+                Component.literal(text).getVisualOrderText(),
+                false,
+                Font.DisplayMode.SEE_THROUGH,
+                state.lightCoords,
+                0xffffffff,
+                0,
+                0
+        );
+
     }
 }
